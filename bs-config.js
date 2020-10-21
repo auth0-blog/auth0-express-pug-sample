9edf34490e9960521d53de6e42ecbb85db9d859a
Dan <dan.arias@auth0.com>
2020-10-21 09:26:33 -0400

    Improve starter layout

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
   proxy: "localhost:4041",
   files: ["**/*.css", "**/*.pug", "**/*.js"],
   ignore: ["node_modules"],
-  reloadDelay: 10,
+  reloadDelay: 300,
   ui: false,
   notify: false,
   port: 4040,
